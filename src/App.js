873f12bc2c195c5cd2bc5739f965819fb7cdc58b
eskalona17 <eskalona_17@hotmail.com>
2020-05-13 16:56:04 +0200

    creating first component and adding skeleton instead bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Formulario from './components/Formulario'
 
 function App() {
-  return (
-    <h1>Hola mundo</h1>
-  );
+    return(
+        <>
+        <h1>Administrador de Pacientes</h1>
+        <div className="container">
+            <div className="row">
+                <div className="one-half column">
+                    <Formulario/>
+                </div>
+                <div className="one-half column">
+                    2
+                </div>
+            </div>
+        </div>
+        </>
+    )
 }
 
-export default App;
+export default App
