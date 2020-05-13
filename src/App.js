5860526ac24993d6e7b6b253751548caa2ea637c
eskalona17 <eskalona_17@hotmail.com>
2020-05-13 17:56:55 +0200

    creating second component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Formulario from "./components/Formulario";
+import Cita from './components/Cita'
 
 function App() {
   //array de citas
@@ -18,7 +19,15 @@ function App() {
           <div className="one-half column">
             <Formulario crearCita={crearCita} />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+              <h2>Administrar citas</h2>
+              {citas.map(cita => (
+                  <Cita 
+                    key={cita.id}
+                    cita={cita}
+                  />
+              ))}
+          </div>
         </div>
       </div>
     </>
