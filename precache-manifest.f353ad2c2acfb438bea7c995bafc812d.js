48420e777e26ea965cbed4eb3cd4043e7d7152fe
eskalona17 <eskalona_17@hotmail.com>
2020-09-05 13:14:15 +0200

    Updates

---
+++ precache-manifest.f353ad2c2acfb438bea7c995bafc812d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c358ee7e17d931cfd43f46c01aaf9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70c5e6b151a256d3ebbc",
+    "url": "./static/css/main.91ecb4fa.chunk.css"
+  },
+  {
+    "revision": "fa491667fd8095ce6e52",
+    "url": "./static/js/2.8397c5f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8397c5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c5e6b151a256d3ebbc",
+    "url": "./static/js/main.2a08f1f1.chunk.js"
+  },
+  {
+    "revision": "cc0860a82924b9092738",
+    "url": "./static/js/runtime-main.e8170ac9.js"
+  }
+]);
